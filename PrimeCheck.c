9afa514000e33cb4af70801395ceaf47eb34e0ea
shuva360 <shuva.debnath.360@gmail.com>
2026-08-21 21:33:13 +0600

    hi

--- PrimeCheck.c
+++ PrimeCheck.c
@@ -5,12 +5,17 @@ int main () {
     scanf("%d", &n);
     int isPrime = 1;
 
-    for(int i=2; i<=n/2; i++) {
-        if(n%i==0){
-            isPrime = 0;
-            break;
-        } 
+    if (n <= 1) {
+        isPrime = 0;
+    } else {
+        for(int i=2; i<=n/2; i++) {
+            if(n%i==0){
+                isPrime = 0;
+                break;
+            } 
+        }
     }
+
     if(isPrime) {
         printf("The number is prime.\n");
     } else {
